5b15cfcc8bf8c15c4399c07ae9ee21300a16e679
JoseNguyen <abc@example.com>
2022-04-02 19:28:42 +0700

    Initial commit

---
+++ src/Buoi1.java
@@ -0,0 +1,14 @@
+public class Buoi1 {
+    public static void main(String[] args) {
+        String name = "Nguyễn Anh Tú";
+        int age = 24;
+        float height = 1.7f;
+        String favorite = "football";
+        double phone = 0352136185d;
+        String address = "District 10";
+        String work = "Jobless";
+        boolean gender = false; //true or false;
+        //true is female and false is male. Cuz female always right.
+
+    }
+}
